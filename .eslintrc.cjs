e817e4058da4a8c7e01cdac4088fcf3196c1059b
King1376 <1185079648@qq.com>
2024-01-05 07:50:55 +0800

    feat: lint-rules

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-essential',
+    'prettier',
+  ],
+  overrides: [
+    {
+      files: ['.js', '.jsx', '.ts', '.tsx', '.vue', '.json'],
+    },
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'vue', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+  },
+  ignorePatterns: ['node_modules/', 'dist/', 'pnpm-lock.yaml', '*.log'],
+};
